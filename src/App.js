394d918fe923692ae1216ef2508790f9e9a539f4
Arsh10x <arshad.a0921@gmail.com>
2024-09-26 15:24:06 +0530

    done with assesment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
+import PopupForm from './components/PopupForm';
 
-function App() {
+const App = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSave = (segmentData) => {
+    console.log('Segment Data:', segmentData); // Logs the segment data
+   // wekhook site not working 
+    handleClose(); // Close the popup after saving
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="contained" onClick={handleOpen}>
+        Save Segment
+      </Button>
+      <PopupForm open={open} onClose={handleClose} onSave={handleSave} />
     </div>
   );
-}
+};
 
 export default App;
